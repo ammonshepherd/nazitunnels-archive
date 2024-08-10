1fe96b6bd7c479ca029dea3801f011b6fd57a769
Ammon Shepherd <ammon.shepherd@gmail.com>
2024-08-10 17:03:29 -0400

    First commit

---
+++ remove-nazitunnels.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# This script removes occurrences of "https://nazitunnels.org/" in HTML <a> and <img> tags
+# from all .html files in the current directory and its subdirectories.
+
+# Set the locale to handle different character encodings
+export LANG=C
+
+# Function to process a single file
+process_file() {
+    local file="$1"
+    # Remove occurrences of the URL in <a> and <img> tags
+    sed -i '' -E 's@<a([^>]*)https://nazitunnels.org/([^>]*)>@<a\1\2>@g; s@<img([^>]*)https://nazitunnels.org/([^>]*)>@<img\1\2>@g' "$file"
+}
+
+export -f process_file
+
+# Find all .html files in the current directory and subdirectories
+find . -type f -name "*.html" -exec bash -c 'process_file "$0"' {} \;
+
+echo "URL removal completed."
+
